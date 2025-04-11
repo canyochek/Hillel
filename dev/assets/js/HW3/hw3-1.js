1d2c103229564223dd5c4686acf5c73608c65ca3
canyochek <aleks47898@gmail.com>
2025-04-11 16:08:24 +0300

    Updated hw3 files

--- dev/assets/js/HW3/hw3-1.js
+++ dev/assets/js/HW3/hw3-1.js
@@ -23,17 +23,3 @@ console.log(typeof exampleFunction);
 
 const uniqueID = Symbol('description');
 console.log(typeof uniqueID)
-
-    // 3.2 Числа та рядки
-
-const firstString = prompt();
-const SecondString = prompt();
-const LastString = prompt();
-console.log(`${firstString}, ${SecondString}, ${LastString}`)
-
-    // 3.3 Розкласти п'ятизначне число
-
-let num = prompt();
-let str = num.split("");
-let result = str.join(" ");
-console.log(result);
---
+++ dev/assets/js/HW3/hw3-2.js
@@ -0,0 +1,6 @@
+    // 3.2 Числа та рядки
+
+const firstString = prompt();
+const SecondString = prompt();
+const LastString = prompt();
+console.log(`${firstString}, ${SecondString}, ${LastString}`)
