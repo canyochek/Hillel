31726372f65cb8ea01a5c3da5425937c723d8679
canyochek <aleks47898@gmail.com>
2025-04-15 19:15:51 +0300

    Update HW4

--- dev/assets/js/HW4/hw4-3.js
+++ dev/assets/js/HW4/hw4-3.js
@@ -10,22 +10,38 @@ if (typeof yearOfBirth === "number" && yearOfBirth != 0) {
     alert(`Шкода, що Ви не захотіли ввести свій рік народження`);
 }
 
-if (cityOfLiving === "Київ" || cityOfLiving === "Вашингтон" || cityOfLiving === "Лондон") {
-    alert(`Ти живеш у столиці ${cityOfLiving}`);
-} else if (!cityOfLiving) {
-    alert(`Шкода, що Ви не захотіли ввести своє місто`);
-} else {
-    alert(`Ти живеш у місті ${cityOfLiving}`);
+switch (true) {
+    case cityOfLiving === "Київ":
+        alert("Ти живеш у столиці України");
+        break;
+    case cityOfLiving === "Вашингтон":
+        alert("Ти живеш у столиці Америки");
+        break;
+    case cityOfLiving === "Лондон":
+        alert("Ти живеш у столиці Британії");
+        break;
+    case !cityOfLiving:
+        alert(`Шкода, що Ви не захотіли ввести своє місто`);
+        break;
+    case typeof cityOfLiving === "string":
+        alert(`Ти живеш у місті ${cityOfLiving}`);
+        break;
 }
 
-if (favouriteSport === "Бодібілдінг") {
-    alert("Круто! Хочеш стати Ронні Коулмен?")
-} else if (favouriteSport === "Бокс") {
-    alert("Круто! Хочеш стати Віталій Кличко?")
-} else if (favouriteSport === "Футбол") {
-    alert("Круто! Хочеш стати Кріштіану Роналду?")
-} else if (!favouriteSport) {
-    alert(`Шкода, що Ви не захотіли ввести улюблений вид спорту`)
-} else {
-    alert ("Непоганий вид спорту!")
+switch (true) {
+    case favouriteSport === "Бодібілдінг":
+        alert("Круто! Хочеш стати Ронні Коулмен?");
+        break;
+    case favouriteSport === "Футбол":
+        alert("Круто! Хочеш стати Кріштіану Роналду?");
+        break;
+    case favouriteSport === "Бокс":
+        alert("Круто! Хочеш стати Віталій Кличко?");
+        break;
+    case !favouriteSport:
+        alert(`Шкода, що Ви не захотіли ввести улюблений вид спорту`);
+        break;
+    case typeof favouriteSport === "string":
+        alert ("Непоганий вид спорту!")
+        break;
 }
