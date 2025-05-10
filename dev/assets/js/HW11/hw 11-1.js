a1547f0a1aa0600868466d88e4882899ad33b799
canyochek <aleks47898@gmail.com>
2025-05-10 14:46:24 +0300

    added HW11

---
+++ dev/assets/js/HW11/hw 11-1.js
@@ -0,0 +1,13 @@
+const table = document.createElement('table')
+for (let i = 1; i <= 10; i++) {
+  const row = document.createElement('tr'); 
+
+  for (let j = 1; j <= 10; j++) {
+    const cell = document.createElement('td'); 
+    cell.textContent = i * j; 
+    row.appendChild(cell); 
+  }
+
+  table.appendChild(row); 
+}
+document.body.appendChild(table)
