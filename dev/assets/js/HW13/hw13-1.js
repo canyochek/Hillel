3dbde6156c97d9d544c9cee03e63e56af1ab81fe
Oleksandr Kovalenko <aleks47898@gmail.com>
2025-07-22 18:48:54 +0300

    update JS HW-13

--- dev/assets/js/HW13/hw13-1.js
+++ dev/assets/js/HW13/hw13-1.js
@@ -1,55 +1,25 @@
 const form = document.querySelector(".inputInfo");
 const giveInfo = form.elements["infoUserButton"];
-const giveUserName = form.elements["infoUserName"];
-const giveMessage = form.elements["infoUserMessage"];
-const givePhone = form.elements["infoUserNumber"];
-const giveEmail = form.elements["infoUserEmail"];
+
+function validateField(selector, checkUser, initialPlaceholder, errorMessage) {
+
+  const field = form.elements[selector];
+  const valueField = field.value.trim();
+
+  if (checkUser.test(valueField)) {
+    console.log(valueField)
+    field.placeholder = initialPlaceholder
+    field.classList.remove("errorResult")
+  } else {
+    field.placeholder = errorMessage
+    field.value = "";
+    field.classList.add("errorResult")
+  }
+}
+
 giveInfo.addEventListener('click', function() {
-  const userName = giveUserName.value.trim();
-  const checkUserName = /^[a-zA-Zа-яА-Я]+$/;
-  if (checkUserName.test(userName)) {
-    console.log(`Name - ${userName}`)
-    giveUserName.placeholder = "Name"
-    giveUserName.classList.remove("errorResult")
-  } else {
-    giveUserName.placeholder = "Введіть своє ім'я"
-    giveUserName.value = "";
-    giveUserName.classList.add("errorResult")
-  }
-
-  const userMessage = giveMessage.value.trim();
-  const checkMessage = /^.{5,}$/
-  if (checkMessage.test(userMessage)) {
-    console.log(`Message: ${userMessage}`)
-    giveMessage.placeholder = "Message"
-    giveMessage.classList.remove("errorResult")
-  } else {
-    giveMessage.placeholder = "Введіть мінімум 5 символів"
-    giveMessage.value = "";
-    giveMessage.classList.add("errorResult")
-  }
-
-  const userPhone = givePhone.value.trim();
-  const checkPhone = /^\+38\d{10}$/
-  if (checkPhone.test(userPhone)) {
-    console.log(`Phone: ${userPhone}`)
-    givePhone.placeholder = "Phone"
-    givePhone.classList.remove("errorResult")
-  } else {
-    givePhone.placeholder = "Номер починається з +38 без пробілів"
-    givePhone.value = "";
-    givePhone.classList.add("errorResult")
-  }
-
-  const userEmail = giveEmail.value.trim();
-  const checkEmail = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/
-  if (checkEmail.test(userEmail)) {
-    console.log(`Email: ${userEmail}`)
-    giveEmail.placeholder = "Email"
-    giveEmail.classList.remove("errorResult")
-  } else {
-    giveEmail.placeholder = "Введіть правильну електронну пошту"
-    giveEmail.value = "";
-    giveEmail.classList.add("errorResult")
-  }
-})
+  validateField("infoUserName", /^[a-zA-Zа-яА-Я]+$/, "Ім'я", "Введіть своє ім'я");
+  validateField("infoUserMessage", /^.{5,}$/, "Повідомлення", "Введіть мінімум 5 символів");
+  validateField("infoUserNumber", /^\+38\d{10}$/, "Номер телефону", "Номер починається з +38 без пробілів");
+  validateField("infoUserEmail", /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/, "Електронна пошта", "Введіть правильну електронну пошту");
+});
